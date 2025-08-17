0fe65523965dcd2990e65f04a278791b802b96ca
Labh Bothra <labhbothra1380@gmail.com>
2025-08-18 00:24:47 +0530

    d

--- bike-inventory-frontend/src/App.js
+++ bike-inventory-frontend/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BikeInventoryDashboard from './pages/Dashboard';
-import AddBike from './pages/AddBike';
-import EditBike from './pages/EditBike';
-import SellRequests from './pages/SellRequests';
-import BuyRequests from './pages/BuyRequests';
-import AdminLogin from './pages/Login';
-import Bookings from './pages/Bookings';
+import React from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BikeInventoryDashboard from "./pages/Dashboard";
+import AddBike from "./pages/AddBike";
+import EditBike from "./pages/EditBike";
+import SellRequests from "./pages/SellRequests";
+import BuyRequests from "./pages/BuyRequests";
+import AdminLogin from "./pages/Login";
+import Bookings from "./pages/Bookings";
 
 function App() {
   // You would typically manage user authentication state here
@@ -18,12 +18,21 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<AdminLogin setUser={setUser} />} />
-  <Route path="/admin/dashboard" element={<BikeInventoryDashboard user={user} />} />
-  <Route path="/admin/bookings" element={<Bookings user={user} />} />
+        <Route
+          path="/admin/dashboard"
+          element={<BikeInventoryDashboard user={user} />}
+        />
+        <Route path="/admin/bookings" element={<Bookings user={user} />} />
         <Route path="/admin/bike/add" element={<AddBike user={user} />} />
         <Route path="/admin/bike/edit/:id" element={<EditBike user={user} />} />
-        <Route path="/admin/sell-requests" element={<SellRequests user={user} />} />
-        <Route path="/admin/quote-requests" element={<BuyRequests user={user} />} />
+        <Route
+          path="/admin/sell-requests"
+          element={<SellRequests user={user} />}
+        />
+        <Route
+          path="/admin/quote-requests"
+          element={<BuyRequests user={user} />}
+        />
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
